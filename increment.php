632e50455a74b59e5c53e5ec2acbd94d5858283d
bouffemadub <stephane.lavigne2608@gmail.com>
2017-03-20 23:54:00 +0100

    Add files via upload
    mise à jours  code fini

--- increment.php
+++ increment.php
@@ -1,12 +1,10 @@
 <?php
 
 //Developpé par stephane.
+//réveil dans 5h bordel, j'ai eu un flash dans la nuit j'ai juste simplifié le code.
 
 $user = "root";
 $pass = "";
-
-//$bdd = new PDO('mysql:host=localhost;dbname=streetmap', $user, $pass);
-//$reponse = $bdd->query('SELECT * from streetmap');
 
 $dbh = new PDO('mysql:host=localhost;dbname=streetmap', $user, $pass);
 $req = $dbh->query('SELECT * from streetmap');
@@ -16,11 +14,13 @@ $f = fopen("villesetmessages.json","w+");
 
 fwrite($f, "{ \r\n \"histoire\": [ \r\n");
 $nmb = $req->rowCount();
+echo $nmb;
 
 $i = 0;
 
 while($donnees = $req->fetch()) {
-    if($i < $nmb )
+    $i++;
+    if($i < $nmb)
     {
         fwrite($f, "{\n
             \"ID\": \"".$donnees['ID']."\",\n
@@ -37,7 +37,7 @@ while($donnees = $req->fetch()) {
             \"ville\": \"".$donnees['ville']."\",\n
             \"coordonees\": ".$donnees['coordonees'].",\n
             \"nombredemessage\": ".$donnees['nbmsg']."\n
-        }");
+        } \n");
     }
 }
 
